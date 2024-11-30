1adb1eff064eec9ebb0fc007021c8885fc6918c4
Curtis Clement <curtis.clement@wonderkind.com>
2024-11-30 16:44:36 +0100

    add form content to new_item.dart file

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_app/data/categories.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -17,7 +18,51 @@ class _NewItemState extends State<NewItem> {
       body: Padding(
         padding: const EdgeInsets.all(12),
         child: Form(
-          child: ListView(),
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: const InputDecoration(
+                  label: Text('Name'),
+                ),
+                validator: (value) {
+                  return 'Must not be empty';
+                },
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text('Quantity'),
+                      ),
+                      initialValue: '1',
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: DropdownButtonFormField(items: [
+                      for (final category in categories.entries)
+                        DropdownMenuItem(
+                        value: category.value,
+                        child: Row(
+                          children: [
+                            Container(
+                              width: 16,
+                              height: 16,
+                              color: category.value.color,
+                            ),
+                            const SizedBox(width: 6),
+                            Text(category.value.title),
+                            ],
+                          ),
+                        ),
+                    ], onChanged: (value) {}),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
